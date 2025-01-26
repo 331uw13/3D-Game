6952f605ae4c72cf719620f014b9be288c8297ae
331uw13 <69628281+331uw13@users.noreply.github.com>
2025-01-26 09:13:57 +0200

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+files=$(find ./src/ -type f -name *.cpp)
+
+name="out"
+compiler_flag="-ggdb"
+
+if [[ $1 == "o" || $2 == "o" ]]; then
+    compiler_flag="-O3"
+fi
+
+echo $files | sed 's/ /\n/g'
+
+if cc -o $name $files $compiler_flag \
+    -Wall \
+    -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 ; then
+ 
+    echo -en "\033[32m"
+    ls -lh $name
+    echo -en "\033[0m"
+
+    if [[ $1 == "r" || $2 == "r" ]]; then
+        ./$name
+    fi
+
+fi
