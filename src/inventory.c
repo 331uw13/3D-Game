06ed4b1d4547ff6d2a12af55395356ccbb97c6c6
331uw13 <69628281+331uw13@users.noreply.github.com>
2025-05-11 13:12:09 +0000

    3d inventory

--- src/inventory.c
+++ src/inventory.c
@@ -27,9 +27,9 @@ void inventory_render(struct state_t* gst, struct inventory_t* inv) {
     struct light_t inv_light = (struct light_t) {
         .type = LIGHT_POINT,
         .enabled = 1,
-        .color = (Color){ 240, 130, 30, 255 },
-        .strength = 1.0,
-        .radius = 2.0,
+        .color = (Color){ 240, 150, 30, 255 },
+        .strength = 0.86,
+        .radius = 3.0,
         .index = INVENTORY_LIGHT_ID,
         .position = gst->player.cam.position
     };
@@ -50,17 +50,43 @@ void inventory_render(struct state_t* gst, struct inventory_t* inv) {
     float fov_x = 2.0 * atan(tan(fov_y / 2.0) * aspect_ratio);
     float frustrum_width = (2.0 * inv_depth * tan(fov_x / 2.0)) / 2.0 - box_size;
 
+    float box_padding = 0.1;
+    float total_boxes_width = (INV_NUM_COLUMNS * (box_size + box_padding));
 
-
+    float x_zeroto_pi = 0.0;
+    float x_zeroto_pi_inc = M_PI / (float)INV_NUM_COLUMNS + 0.3;
 
     for(int y = 0;  y < INV_NUM_ROWS; y++) {
         for(int x = 0; x < INV_NUM_COLUMNS; x++) {
+            
+            float depth_offset = 1.0 - (0.5+0.5*sin(x_zeroto_pi*1.3+M_PI/2.0));
+            depth_offset = pow(depth_offset, 0.5);
+            x_zeroto_pi += x_zeroto_pi_inc;
 
-            Matrix offset    = MatrixTranslate(xf-frustrum_width, yf-1.0, -inv_depth);
-            Matrix rotation  = MatrixRotateY(gst->time + xf*0.1);
-            Matrix scale     = MatrixScale(box_scale, box_scale, box_scale);
+            Matrix offset = MatrixTranslate(
+                    xf-(frustrum_width/2)-box_size/2,
+                    yf-1.0,
+                    -(inv_depth + depth_offset*0.35));
 
+            float roty = 0.0;
+            float rotx = 0.0;
 
+            // Calculate rotation angle for box.
+            // inverted camera matrix has to be multiplied with offset first
+            // to get box position.
+            {
+                Matrix tmp = MatrixMultiply(icam_matrix, offset);
+                Vector3 angles = get_rotation_yz(
+                        gst->player.cam.position, (Vector3){ tmp.m12, tmp.m13, tmp.m14 });
+                roty = angles.y + M_PI/2;
+                rotx = -angles.z;
+            }
+
+            Matrix rotation  = MatrixIdentity();
+            rotation = MatrixMultiply(rotation, MatrixRotateX(rotx));
+            rotation = MatrixMultiply(rotation, MatrixRotateY(roty));
+
+            Matrix scale = MatrixScale(box_scale, box_scale, box_scale);
             Matrix current = MatrixMultiply(rotation, MatrixMultiply(scale, offset));
             current = MatrixMultiply(current, icam_matrix);
 
@@ -69,9 +95,10 @@ void inventory_render(struct state_t* gst, struct inventory_t* inv) {
                 box->materials[0],
                 current);
 
-            xf += box_size;
+            xf += box_size + box_padding;
         }
-        yf += box_size;
+        x_zeroto_pi = 0.0;
+        yf += box_size + box_padding;
         xf = 0.0;
     }
 
